bb1656ea8570f10f7b49baa5cfccb44280202dc4
querlemtle <citta120120@gmail.com>
2022-08-08 05:45:24 +0800

    fix: deploy.sh setting

--- deploy.sh
+++ deploy.sh
@@ -21,6 +21,7 @@ git commit -m 'deploy'
 
 # if you are deploying to https://<USERNAME>.Github.io/<REPO>
 # git push -f https://github.com/<USERNAME>/<REPO>.git main:gh-pages
+git branch -M main
 git push -f https://github.com/querlemtle/twitter-vue-front-end.git main:gh-pages
 
 cd -
